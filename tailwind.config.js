5d144649cd6a5d7fee7170f60347fb08bc4b7f0e
Seth Vergeyle <github@vergeyle.net>
2020-05-28 19:33:28 -0500

    Change font stack to IBM Plex

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'mono': ['"IBM Plex Mono"', '"Menlo"', '"DejaVu Sans Mono"', '"Bitstream Vera Sans Mono"', 'Courier', 'monospace'],
+      'sans': ['"IBM Plex Sans"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+      'serif': ['"IBM Plex Serif"', 'Georgia', 'Times', 'serif']
+    }
   },
   variants: {},
   plugins: [],
